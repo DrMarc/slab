406ad79ca2392c15294ae41ff9e9676f1701696c
Marc Schoenwiesner <marc.schoenwiesner@gmail.com>
2019-06-08 20:45:17 +0200

    v0.4 -> v0.5

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 def readme():
 	with open('README.rst') as f:
 		return f.read()
-		
+
 setup(name='slab',
-	version='0.4',
+	version='0.5',
 	description='Tools for generating and manipulating digital signals, particularly sounds.',
 	url='http://github.com/',
 	author='Marc Schoenwiesner',
