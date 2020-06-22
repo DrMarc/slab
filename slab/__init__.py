74e780c5b84d09caa9ed80fedeffa134e079a440
Marc Schoenwiesner <marc.schoenwiesner@gmail.com>
2020-06-22 11:53:48 +0200

    autopep8 broke DATAPATH definition, corrected

--- slab/__init__.py
+++ slab/__init__.py
@@ -1,11 +1,11 @@
 # install from github:
 # pip install git+git://github.com/DrMarc/soundtools.git
 
+import pathlib
+import os
+DATAPATH = str(pathlib.Path(__file__).parent.resolve() / pathlib.Path('data')) + os.sep
 from slab.hrtf import *
 from slab.psychoacoustics import *
 from slab.binaural import *
 from slab.sound import *
 from slab.signals import *
-import pathlib
-import os
-DATAPATH = str(pathlib.Path(__file__).parent.resolve() / pathlib.Path('data')) + os.sep
