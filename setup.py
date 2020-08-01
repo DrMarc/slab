6b3afd21f28bfe6306e80d28945703f62c9065cc
Marc Schoenwiesner <marc.schoenwiesner@gmail.com>
2020-08-01 15:25:57 +0200

    renamed the build module to avoid conflict on PyPI

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
 
-def readme():
-	with open('README.rst') as f:
-		return f.read()
+with open('README.rst') as f:
+    readme = f.read()
 
-setup(name='slab',
+setup(name='soundtools',
 	version='0.6',
 	description='Tools for generating and manipulating digital signals, particularly sounds.',
+	long_description=readme,
 	url='http://github.com/DrMarc/soundtools.git',
 	author='Marc Schoenwiesner',
 	author_email='marc.schoenwiesner@gmail.com',
 	license='MIT',
+	python_requires='>=3.6',
 	packages=['slab'],
 	package_data={'slab': ['data/mit_kemar_normal_pinna.sofa', 'data/KEMAR_interaural_level_spectrum.npy']},
 	include_package_data=True,
