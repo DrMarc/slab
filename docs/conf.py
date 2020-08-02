4e738e387644a64c28ce74ad50a3d07cd6f64ac1
Marc Schoenwiesner <marc.schoenwiesner@gmail.com>
2020-08-02 18:52:51 +0200

    update for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -21,28 +21,30 @@ project = 'slab'
 copyright = '2020, Marc Schoenwiesner, Ole Bialas'
 author = 'Marc Schoenwiesner, Ole Bialas'
 
-# The full version, including alpha/beta/rc tags
-release = '0.7'
-
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_default_options = {
-    'member-order': 'bysource'
-}
+autodoc_default_options = {'member-order': 'bysource'}
+
+intersphinx_mapping = {'python': ('https://docs.python.org/', None),
+                       'matplotlib': ('http://matplotlib.org/', None),
+                       'numpy': ('http://docs.scipy.org/doc/numpy/', None),
+                       'scipy': ('http://docs.scipy.org/doc/scipy/reference/', None)}
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
