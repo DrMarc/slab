04b36253000b2712abb8409b66b2226495f8fb60
Ole <bialas.ole@gmx.de>
2020-08-04 10:03:50 +0200

    pytest should work now

--- slab/tests/test_sounds.py
+++ slab/tests/test_sounds.py
@@ -1,4 +1,7 @@
-import pytest
+import sys
+from pathlib import Path
+_location = Path(__file__).resolve().parents[2]  # append the main folder
+sys.path.append(_location)
 
 
 def test_generating_sounds():
