16fa847d69e43144ee0402782354432bd880bb4b
OleBialas <bialas.ole@gmx.de>
2020-09-03 12:09:39 +0200

    make list of None elementrs fro response

--- tests/test_psychoacoustics.py
+++ tests/test_psychoacoustics.py
@@ -1,22 +1,50 @@
 import slab
-import curses
-"""
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
-# yield stdscr
-curses.nocbreak()
-curses.echo()
-curses.endwin()
-stdscr.keypad(True)
-"""
+import numpy
+
 def test_trialsequence():
 
-    seq = slab.Trialsequence(conditions=5, n_reps=100, kind="non_repeating")
+    seq = slab.Trialsequence(conditions=5, kind="infinite")
+    for i in range(10):
+        seq.__next__()
+        seq.get_future_trial(i)
+
+    kinds = ["non_repeating", "random_permutation"]
+    for kind in kinds:
+        seq = slab.Trialsequence(conditions=5, n_reps=10, kind=kind)
+        seq.transitions()
+        seq.condition_probabilities()
+        for trial in seq:
+            seq.add_response(seq.simulate_response(threshold=1))
+        assert seq.response_summary() is not None
+        seq.plot()
+
+    deviant_freq = 0.15
+    seq = slab.Trialsequence(conditions=1, n_reps=1000,
+        deviant_freq=deviant_freq, kind="mismatch_negativity")
+    sum(seq.trials)/len(seq.trials)
+
+
+def test_mmnsequence():
+    deviant_freq = 0.15
+    n_trials = 100
+    seq = slab.Trialsequence.mmn_sequence(n_trials=110, deviant_freq=deviant_freq)
+    sum(seq.trials)
+
+    n_partials = int(numpy.ceil((2 / deviant_freq) - 7))
+    reps = int(numpy.ceil(n_trials/n_partials))
+
+    partials = []
+    for i in range(n_partials):
+        partials.append([0] * (3+i) + [1])
+    print(partials)
+
+    idx = list(range(n_partials)) * reps
+    numpy.random.shuffle(idx)
+
 
 # def test_keyinput():
-#    seq.present_afc_trial(target=slab.Sound.tone(), distractors=slab.Sound.whitenoise())
-
+# Seems like curses does not run in pytest - is there a workaround?
 #    with slab.Key() as key:
-#        response = key.getch()  # how to test this if there is no key input?
-#    slab.psychoacoustics.input_method = 'buttonbox'
+#        response = key.getch()
+#    print(response)
+#    seq.present_afc_trial(target=slab.Sound.tone(), distractors=slab.Sound.whitenoise())
