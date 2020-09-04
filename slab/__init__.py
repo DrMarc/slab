0ed3f78284501392f37f9ff0b10c5f98ba84c8af
OleBialas <bialas.ole@gmx.de>
2020-09-04 15:51:00 +0200

    fix import order

--- slab/__init__.py
+++ slab/__init__.py
@@ -1,12 +1,12 @@
 
+import pathlib
+import os
+import sys
+sys.path.append('..\\')
+DATAPATH = str(pathlib.Path(__file__).parent.resolve() / pathlib.Path('data')) + os.sep
 from slab.signal import *
 from slab.sound import *
 from slab.binaural import *
 from slab.psychoacoustics import *
 from slab.hrtf import *
 from _version import __version__
-import pathlib
-import os
-import sys
-sys.path.append('..\\')
-DATAPATH = str(pathlib.Path(__file__).parent.resolve() / pathlib.Path('data')) + os.sep
