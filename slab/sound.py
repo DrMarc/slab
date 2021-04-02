f9e1fe2d949c50f0db3ba29084c254160ce1e81a
Marc Schoenwiesner <marc.schoenwiesner@gmail.com>
2021-04-02 20:16:20 +0200

    clamped window duration in frames() to > 7 samples

--- slab/sound.py
+++ slab/sound.py
@@ -1075,7 +1075,7 @@ class Sound(Signal):
         """ A generator that steps through the sound in overlapping, windowed frames.
         Get the frame center times by calling Sound's `frametimes` method.
         Arguments:
-            duration (int | float): half of the length of the returned frames in samples (int) or seconds (float)
+            duration (int | float): half of the length of the returned frames in samples (int) or seconds (float), must be larger than 7 samples.
         Returns:
             (generator): the generator object that yields frames which are of the same type as the object.
         Examples:
@@ -1086,7 +1086,7 @@ class Sound(Signal):
         frame = copy.deepcopy(self)
         if scipy is False:
             raise ImportError('Need scipy for time window processing.')
-        window_nsamp = Sound.in_samples(duration, self.samplerate) * 2
+        window_nsamp = min(15, Sound.in_samples(duration, self.samplerate) * 2)
         # step duration optimal for Gaussian windows
         step_nsamp = numpy.floor(window_nsamp / numpy.sqrt(numpy.pi) / 8).astype(int)
         # make the window, Gaussian filter needs a minimum of 6σ - 1 samples.
@@ -1104,10 +1104,10 @@ class Sound(Signal):
     def frametimes(self, duration=1024):
         """ Returns the time points at the frame centers constructed by the `frames` method.
         Arguments:
-            duration (int | float): half of the length of the returned frames in samples (int) or seconds (float)
+            duration (int | float): half of the length of the returned frames in samples (int) or seconds (float), must be larger than 7 samples.
         Returns:
             (numpy.ndarray): the center of each frame in seconds. """
-        window_nsamp = Sound.in_samples(duration, self.samplerate) * 2
+        window_nsamp = min(15, Sound.in_samples(duration, self.samplerate) * 2)
         step_nsamp = numpy.floor(window_nsamp / numpy.sqrt(numpy.pi) / 8).astype(int)
         samplepoints = []
         idx = 0
