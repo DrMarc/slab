259b40d033b38fb0c7b32c8714233abe83d2ce90
Chao Huang <c.huang03@gmail.com>
2022-07-04 15:21:57 +0200

    small changes to make the hrtf test less stringent (and less likely to fail due to RNG) (#61)
    * bug fix on Filter.equalizing_filterbank; add functionality to generate filter with difference amplification/attunuation level so it can be used in different settings, e.g. filtfilt vs. causal filtering
    
    * bug fix on Test/test_filter.test_equalization; removed gustav padding on filtering, because it consume too much RAM. Test/test_hrtf is still problematic
    
    * bug fix test/test_filter and test/test_hrtf; updated testing logics
    
    * bugfix test/test_hrtf: make the criterion less stringent so less RNG-dependent; add a convinient method to check the calibration intensity

--- slab/__init__.py
+++ slab/__init__.py
@@ -8,7 +8,7 @@ sys.path.append('..\\')
 from slab.hrtf import HRTF
 from slab.psychoacoustics import *
 from slab.binaural import Binaural
-from slab.sound import Sound, set_default_level, set_calibration_intensity, calibrate
+from slab.sound import Sound, set_default_level, set_calibration_intensity, get_calibration_intensity, calibrate
 from slab.signal import Signal, set_default_samplerate
 from slab.filter import Filter
 
